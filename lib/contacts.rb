aa0f2736a135b4fb8c6bfd682a8ddfd4b3161397
Milos Popovic <milos_popovic@outlook.com>
2020-07-16 04:30:43 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,19 +1,25 @@
 require 'pry'
 
-  # This is the array we will be passing into the remove_strawberry method
-  # contacts = {
-  #   "Jon Snow" => {
-  #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
-  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-  #   },
-  #   "Freddy Mercury" => {
-  #     name: "Freddy",
-  #     email: "freddy@mercury.com",
-  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-  #   }
-  # }
+  This is the array we will be passing into the remove_strawberry method
+  contacts = {
+    "Jon Snow" => {
+      name: "Jon",
+      email: "jon_snow@thewall.we", 
+      favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+    },
+    "Freddy Mercury" => {
+      name: "Freddy",
+      email: "freddy@mercury.com",
+      favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+    }
+  }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |name, attribute|
+    attribute.each do |data, value|
+      if data == :favorite_ice_cream_flavors == "strawberry"
+        data.pop()
+      end
+    end
+  end
 end
